fc5180e48c4455c85348f8021b0d38d7296394f8
mdambrosioultravi <mdambrosio@ultravi.com>
2023-02-16 11:20:17 -0500

    The server was not handling an invalid token on login, fixed the small issue

--- app.js
+++ app.js
@@ -700,6 +700,12 @@ app.post('/auth/local', function (req, res) {
 app.post('/auth/authenticateRequest', async function (req, res) {
     isValidResponse = await isTokenValid(req.body.loginToken);
 
+    if(isValidResponse.response == false){
+        // Token not valid, handle:
+        res.json({ 'response': false });
+        return;
+    }
+
     // API Route to get all tasks available to the ID passed in the parameter.
     connectionPool.query(allTasksAvailableToUserById, [isValidResponse.data._id], (err, results) => { //Hard Coding the ID to 1 for testing
         if (err) {
