5955b9df140a931f81ed34ae486c3ef7f493fae4
mdambrosioultravi <mdambrosio@ultravi.com>
2023-05-30 13:46:28 -0400

    Started implementing new method to get flight activity RULES. these rules are now being created from date and from arrival/departure times.

--- api/flightSchedule/index.js
+++ api/flightSchedule/index.js
@@ -11,76 +11,6 @@ const moment = require('moment');
 
 // =================== Middlewares ===========================
 
-function replaceSpecialCharacters(str) {
-    const regex = /['"\\\0\\\r\\\n\\\t\\\v\\\b\\\f&<>%*;[\]{}!:#^$~|\/?]/g;
-    return str.replace(regex, "");
-}
-
-// Middleware function to ensure filename does not contain spaces nor any '.pdf'
-const sanitizeData = function (req, res, next) {
-    // Remove any instance of .pdf from filename
-    let newFilename = req.body.formFilename;
-    newFilename = newFilename.replace(/\.pdf/g, ''); // remove any instance of '.pdf' from the filename
-    req.body.formFilename = newFilename;
-
-    req.body.formDocname = replaceSpecialCharacters(req.body.formDocname);
-    req.body.formFilenameSanitized = replaceSpecialCharacters(req.body.formFilename);
-    req.body.formTitle = replaceSpecialCharacters(req.body.formTitle);
-    req.body.formVersion = replaceSpecialCharacters(req.body.formVersion);
-
-    next();
-}
-
-// Middleware function to save the actual entry on our document database
-const saveToDatabase = function(req, res, next) {
-    console.log('saveToDatabase: ', req.body);
-    // Set our default value for ver field
-    if(req.body.formVersion == '') req.body.formVersion = 'None';
-
-    connectionPool.query(config.queries.addDocumentQuery,
-        [
-            0, // Seq
-            req.body.formDocname,
-            req.body.formFilenameSanitized +  '.pdf',
-            req.body.formTitle,
-            req.body.formVersion,
-            1, // active
-            req.body.formEffective,
-            req.body.formUpdated
-        ], (err, response) => {
-        if (err) {
-            console.log("Query Error: ", err);
-            return res.status(500).send({ message: 'Internal Server Error' });
-        }
-        // console.log("File added successfully");
-        next();
-    });
-}
-
-// Middleware function that ensures that the username and email from the request are:
-// - Active,
-// - Username and Email match
-// This is to reduce spam and ensure reliability of the system.
-const isUserValid = function(req, res, next) {
-    connectionPool.query('SELECT COUNT(*) AS count FROM users WHERE username = ? AND email = ? AND active = ?;' , [req.body.username, req.body.email, 'Y'], (err, rows) => {
-        if (err) {
-            console.log("Query Error: ", err);
-            return res.status(500).send({ message: 'Internal Server Error' });
-        }
-
-        // check if an entry exists
-        if (rows[0].count > 0) {
-            // an entry exists for the matching set of username and email
-            console.log('entry exists for that user AND they are valid');
-            next();
-        } else {
-            // no entry exists
-            console.log('reject the operation.')
-            return res.status(403).send({ message: 'User could not be validated' });
-        }  
-    });
-}
-
 /**
  * Route that retrieves all of the entries from our rules table.
  * We will do the filtering client-side.
@@ -148,226 +78,67 @@ router.post("/createRule", auth.authenticateRequest(20), multer().none(), async 
 });
 
 /**
- * Route that retrieves all of the departures on a given date.
+ * Route that retrieves all of the legs on a given date from flightActivity table
  * auth request 22 is view flight activity.
+ * @param from the timestamp that we will use as the lower bound of our query for dates. From < date we want < Until
+ * @param until the timestamp that we will use as the upper bound of our query for dates. From < date we want < Until
  */
-router.get("/getFlightActivityDepartures/:date", auth.authenticateRequest(22), async (req, res) => {
-    // console.log('unix timestamp passed as req params date: ',req.params.date);
+router.post("/getFlightActivity", auth.authenticateRequest(22), async (req, res) => {
+    console.log('Request.body: ', JSON.stringify(req.body));
+    res.status(200).send({'message': 'activity'});
+});
 
-    // Get day of the week
-    const dayOfWeek = moment(req.params.date * 1000).format('dddd').toLowerCase();
+/**
+ * Route that retrieves all of the legs on a given date from flightBuffer table
+ * auth request 22 is view flight activity.
+ * @param from the timestamp that we will use as the lower bound of our query for dates. From < date we want < Until
+ * @param until the timestamp that we will use as the upper bound of our query for dates. From < date we want < Until
+ */
+router.post("/getFlightBuffer", auth.authenticateRequest(22), async (req, res) => {
+    console.log('Request.body: ', JSON.stringify(req.body));
+    res.status(200).send({'message': 'buffer'});
+});
 
-    console.log('day of week: ', dayOfWeek);
-    
-    // We hard coded MIA into the query
-
+/**
+ * Route that retrieves all of the legs on a given date from flightRules table
+ * auth request 22 is view flight activity.
+ * @param from the timestamp that we will use as the lower bound of our query for dates. From < date we want < Until
+ * @param until the timestamp that we will use as the upper bound of our query for dates. From < date we want < Until
+ */
+router.post("/getFlightRules", auth.authenticateRequest(22), async (req, res) => {
     // Check if the user is logged in, and if his token is valid, If so, find all tasks they have access    to
     jwt.verify(req.headers.logintoken, config.privateKey, (err, decoded) => {
+        // If there is a bad token, reject the request.
         if (err || decoded == undefined) {
             return res.status(500).send({ message: 'Bad Token' });
             
         }
 
-        // Not using the config query
-        const query = `SELECT * FROM ultravi_ulav.flight_schedule_rules WHERE ${req.params.date} BETWEEN date_start AND date_end AND ${dayOfWeek} = true AND departure_city = 'MIA'`;
+        // =============== For DayOfWeek ====================
 
+        // Get mid day of requested day. This will be to get the day of the week of the requested Date (On Local time for the server, which should match station Locale)
+        let middleOfDay = (req.body.from + req.body.until)/2;
+        // Create a moment object from the timestamp (converted into milliseconds)
+        const date = moment(middleOfDay * 1000);
+        // Get the day of the week
+        let dayOfWeek = date.format('dddd');
+        // =============== For utcOffset ====================
+        let offsetMinutes = moment().utcOffset();
+
+
+        // let query = `SELECT * FROM ultravi_ulav.flight_schedule_rules WHERE ${middleOfDay} BETWEEN date_start AND date_end AND ${dayOfWeek} = true AND (arrival_city = 'MIA' OR departure_city = 'MIA');`
+        let query = `SELECT * FROM ultravi_ulav.flight_schedule_rules WHERE (arrival_city = 'MIA'  AND (${req.body.from} + (HOUR(scheduled_arrival_time)* 3600) + (MINUTE(scheduled_arrival_time) * 60) + (${offsetMinutes} * 60) BETWEEN date_start AND date_end) AND ${dayOfWeek} = true) OR (departure_city = 'MIA'  AND (${req.body.from} + (HOUR(scheduled_departure_time)* 3600) + (MINUTE(scheduled_departure_time) * 60) + (${offsetMinutes} * 60) BETWEEN date_start AND date_end) AND ${dayOfWeek} = true);`
+        console.log('Query: ', query);
         connectionPool.query(query, (err, response) => {
             if (err) {
                 console.log("Query Error: ", err);
                 return res.status(500).send({ message: 'Internal Server Error' });
             }
-            // console.log(response);
+
+            console.log(response);
             return res.status(200).send(response);
-        });  
-    })
-});
-
-/**
- * Route that retrieves all of the departures on a given date.
- * auth request 22 is view flight activity.
- */
-router.get("/getFlightActivityArrivals/:date", auth.authenticateRequest(22), async (req, res) => {
-    // console.log('unix timestamp passed as req params date: ',req.params.date);
-
-    // Get day of the week
-    const dayOfWeek = moment(req.params.date * 1000).format('dddd').toLowerCase();
-
-    console.log('day of week: ', dayOfWeek);
-    
-    // We hard coded MIA into the query
-
-    // Check if the user is logged in, and if his token is valid, If so, find all tasks they have access    to
-    jwt.verify(req.headers.logintoken, config.privateKey, (err, decoded) => {
-        if (err || decoded == undefined) {
-            return res.status(500).send({ message: 'Bad Token' });
-            
-        }
-
-        // Not using the config query
-        const query = `SELECT * FROM ultravi_ulav.flight_schedule_rules WHERE ${req.params.date} BETWEEN date_start AND date_end AND ${dayOfWeek} = true AND arrival_city = 'MIA'`;
-
-        connectionPool.query(query, (err, response) => {
-            if (err) {
-                console.log("Query Error: ", err);
-                return res.status(500).send({ message: 'Internal Server Error' });
-            }
-            // console.log(response);
-            return res.status(200).send(response);
-        });  
-    })
-});
-
-/**
- * Route that retrieves all of the departures on a given date.
- * auth request 22 is view flight activity.
- * @param date: the date we are getting is a LOCAL unix timestamp. we must convert it to UTC and then to the startOf('day'); 
- */
-router.get("/getFlightActivity/:date", auth.authenticateRequest(22), async (req, res) => {
-    // Get day of the week (For the Query)
-    console.log('Unedited date: ', req.params.date);
-    let localMoment = moment.unix(req.params.date)
-    console.log('localMoment: ', localMoment);
-
-    // Get the UTC offset for the local timestamp
-    const utcOffsetSeconds = localMoment.utcOffset() * 60;
-    console.log('utcOffsetSeconds: ', utcOffsetSeconds);
-
-    // Convert the local timestamp to UTC timestamp by subtracting the UTC offset
-    const utcTimestamp = localMoment.unix() - utcOffsetSeconds;
-
-    console.log('utcTimestamp', utcTimestamp); // Output: 
-
-
-    // Adding 4 hours to the date so we have local date. Yeah these dates are confusing.
-    let utcOffset = getUtcOffsetInHours();
-    console.log('offset: ', utcOffset)
-    const dayOfWeek = moment.unix(parseInt(req.params.date) + (3600 * utcOffset)).format('dddd').toLowerCase();
-    console.log('day of week: ', dayOfWeek);
-
-    // Input needs to be mutliplied by 1000 to be used. We need to 
-    // Know what category the request falls into, either: FA, FB, FR
-    // past to today -> Flight Activity
-    // (today + 1) to (today + 14)  2 week buffer periond -> Flight Buffer
-    // (today + 15) and above -> Flight Rules.
-    // Assume the Unix timestamp is stored in a variable called `timestamp`.
-
-    const secondsPerDay = 86400;
-    const date = moment.unix(req.params.date).unix();
-    console.log(date);
-    const today = moment().utc().startOf('day').unix();
-    console.log(today)
-    const timestamp = moment.utc().startOf('day').unix();
-    console.log(timestamp); // output: 1672393600
-    
-
-    // Calculate the difference between the two dates in days.
-    const diffInSeconds = (date - today);
-    const diffInDays = Math.floor(diffInSeconds / secondsPerDay);
-    console.log(diffInDays);
-
-    switch (true) {
-    case diffInDays < 0:
-        // Get Flight Buffer Query
-        // We hard coded MIA into the query, 
-        // Check if the user is logged in, and if his token is valid, If so, find all tasks they have access
-
-        jwt.verify(req.headers.logintoken, config.privateKey, (err, decoded) => {
-            if (err || decoded == undefined) {
-                return res.status(500).send({ message: 'Bad Token' });
-                
-            }
-            // Not using the config query
-            const query = `SELECT * FROM ultravi_ulav.flight_schedule_activity WHERE ${req.params.date} BETWEEN date AND (date + 86399)  AND (arrival_city = 'MIA'  OR departure_city = 'MIA');`
-            console.log(query)
-            connectionPool.query(query, (err, response) => {
-                if (err) {
-                    console.log("Query Error: ", err);
-                    return res.status(500).send({ message: 'Internal Server Error' });
-                }
-                console.log(response);
-                return res.status(200).send(response);
-            });  
-        })
-        console.log('Case 1: date is before today.');
-
-
-        break;
-    case diffInDays == 0:
-        console.log('Case 2: date is today.');
-
-        // Get Flight Buffer Query
-        // We hard coded MIA into the query, 
-        // Check if the user is logged in, and if his token is valid, If so, find all tasks they have access
-
-        jwt.verify(req.headers.logintoken, config.privateKey, (err, decoded) => {
-            if (err || decoded == undefined) {
-                return res.status(500).send({ message: 'Bad Token' });
-                
-            }
-            // Not using the config query
-            const query = `SELECT * FROM ultravi_ulav.flight_schedule_activity WHERE ${req.params.date} BETWEEN date AND (date + 86399)  AND (arrival_city = 'MIA'  OR departure_city = 'MIA');`
-            console.log(query)
-            connectionPool.query(query, (err, response) => {
-                if (err) {
-                    console.log("Query Error: ", err);
-                    return res.status(500).send({ message: 'Internal Server Error' });
-                }
-                console.log(response);
-                return res.status(200).send(response);
-            });  
-        })
-
-        break;
-    case diffInDays >= 1 && diffInDays <= 14:
-        // console.log('Case 2: date is between tomorrow and two weeks from now.');
-        
-        // Get Flight Buffer Query
-        // We hard coded MIA into the query, 
-        // Check if the user is logged in, and if his token is valid, If so, find all tasks they have access
-
-            jwt.verify(req.headers.logintoken, config.privateKey, (err, decoded) => {
-                if (err || decoded == undefined) {
-                    return res.status(500).send({ message: 'Bad Token' });
-                    
-                }
-                // Not using the config query
-                const query = `SELECT * FROM ultravi_ulav.flight_schedule_buffer WHERE ${req.params.date} BETWEEN date AND (date + 86399)  AND (arrival_city = 'MIA'  OR departure_city = 'MIA');`
-                console.log(query)
-                connectionPool.query(query, (err, response) => {
-                    if (err) {
-                        console.log("Query Error: ", err);
-                        return res.status(500).send({ message: 'Internal Server Error' });
-                    }
-                    console.log(response);
-                    return res.status(200).send(response);
-                });  
-            })
-        break;
-    case diffInDays >= 15:
-        // console.log('Case 3: date is more than two weeks from now.');
-
-        // Get Flight Rules Query
-        // We hard coded MIA into the query, 
-        // Check if the user is logged in, and if his token is valid, If so, find all tasks they have access
-        jwt.verify(req.headers.logintoken, config.privateKey, (err, decoded) => {
-            if (err || decoded == undefined) {
-                return res.status(500).send({ message: 'Bad Token' });
-                
-            }
-            // Not using the config query
-            const query = `SELECT * FROM ultravi_ulav.flight_schedule_rules WHERE ${req.params.date} BETWEEN date_start AND date_end AND ${dayOfWeek} = true  AND (arrival_city = 'MIA'  OR departure_city = 'MIA');`;
-
-            connectionPool.query(query, (err, response) => {
-                if (err) {
-                    console.log("Query Error: ", err);
-                    return res.status(500).send({ message: 'Internal Server Error' });
-                }
-                // console.log(response);
-                return res.status(200).send(response);
-            });  
-        })
-        break;
-    }
+        }); 
+    });
 });
 
 function getUtcOffsetInHours() {
