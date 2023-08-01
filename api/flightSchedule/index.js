428fdb3a5de98687d2a6268748b4b6dfa6b5fc85
mdambrosioultravi <mdambrosio@ultravi.com>
2023-08-01 14:35:51 -0400

    Dirty commit trying to get add flight to work. What im going to do is actually make create rule add flight to activity as well as now it only adds to buffer

--- api/flightSchedule/index.js
+++ api/flightSchedule/index.js
@@ -837,41 +837,45 @@ router.post("/createFlightLeg", multer().none(), async (req, res) => { // , auth
         
 
         let databaseName = '';
+        let tablename = '';
         console.log(req.body);
         if(req.body.origin == 'activity'){
             databaseName = 'ultravi_ulav.flight_schedule_activity';
+            tablename = 'flight_schedule_activity';
         } else if(req.body.origin == 'buffer'){
             databaseName = 'ultravi_ulav.flight_schedule_buffer';
+            tablename = 'flight_schedule_buffer';
         }
-
-        let query = 
-        `
-            INSERT INTO ${databaseName}
-                (ac_type, actual_arrival_time, actual_departure_time, ac_reg, airline, arrival_city, client, date, departure_city, estimated_arrival_time, estimated_departure_time, flight_number, gate, next_leg_pointer, pax, remarks, scheduled_arrival_time, scheduled_departure_time, wheelchair_count, isSubservice, flightStatus)
-            VALUES
-            (
-                ${parseInt(req.body.ac_type, 10)}, 
-                ${req.body.actual_arrival_time === '' ? 'NULL' : startOfDayArrival.unix() + moment.duration(req.body.actual_arrival_time).asSeconds()},
-                ${req.body.actual_departure_time === '' ? 'NULL' : startOfDayDeparture.unix() + moment.duration(req.body.actual_departure_time).asSeconds()}, 
-                ${req.body.ac_reg !== 'null' && req.body.ac_reg !== '' ? `'${req.body.ac_reg}'` : 'NULL'},
-                ${parseInt(req.body.airline, 10)}, 
-                '${req.body.arrival_city}', 
-                '${req.body.client}', 
-                ${req.body.date}, 
-                '${req.body.departure_city}', 
-                ${req.body.estimated_arrival_time === '' ? 'NULL' : startOfDayArrival.unix() + moment.duration(req.body.estimated_arrival_time).asSeconds()}, 
-                ${req.body.estimated_departure_time === '' ? 'NULL' :startOfDayDeparture.unix() + moment.duration(req.body.estimated_departure_time).asSeconds()}, 
-                ${req.body.flight_number}, 
-                ${req.body.gate !== 'null' ? `'${req.body.gate}'` : 'NULL'}, 
-                ${req.body.next_leg_pointer !== 'null' ? `'${req.body.next_leg_pointer}'` : 'NULL'}, 
-                ${req.body.pax !== 'null' && req.body.pax !== '' ? `'${req.body.pax}'` : 'NULL'},
-                '${req.body.remarks}', 
-                ${req.body.scheduled_arrival_time === '' ? 'NULL' : startOfDayArrival.unix() + moment.duration(req.body.scheduled_arrival_time).asSeconds()}, 
-                ${req.body.scheduled_departure_time === '' ? 'NULL' : startOfDayArrival.unix() + moment.duration(req.body.scheduled_departure_time).asSeconds()},
-                ${req.body.wheelchair_count !== 'null' && req.body.wheelchair_count !== '' ? `'${req.body.wheelchair_count}'` : 'NULL'},
-                ${req.body.isSubservice},
-                ${req.body.flightStatus}
-        );`
+        let query = `
+        INSERT INTO ${databaseName}
+        (ac_type, actual_arrival_time, actual_departure_time, ac_reg, airline, arrival_city, client, date, departure_city, estimated_arrival_time, estimated_departure_time, flight_number, gate, next_leg_pointer, pax, remarks, scheduled_arrival_time, scheduled_departure_time, wheelchair_count, isSubservice, flightStatus, generated_id)
+        SELECT
+            ${parseInt(req.body.ac_type, 10)}, 
+            ${req.body.actual_arrival_time === '' ? 'NULL' : startOfDayArrival.unix() + moment.duration(req.body.actual_arrival_time).asSeconds()},
+            ${req.body.actual_departure_time === '' ? 'NULL' : startOfDayDeparture.unix() + moment.duration(req.body.actual_departure_time).asSeconds()}, 
+            ${req.body.ac_reg !== 'null' && req.body.ac_reg !== '' ? `'${req.body.ac_reg}'` : 'NULL'},
+            ${parseInt(req.body.airline, 10)}, 
+            '${req.body.arrival_city}', 
+            '${req.body.client}', 
+            ${req.body.date}, 
+            '${req.body.departure_city}', 
+            ${req.body.estimated_arrival_time === '' ? 'NULL' : startOfDayArrival.unix() + moment.duration(req.body.estimated_arrival_time).asSeconds()}, 
+            ${req.body.estimated_departure_time === '' ? 'NULL' :startOfDayDeparture.unix() + moment.duration(req.body.estimated_departure_time).asSeconds()}, 
+            ${req.body.flight_number}, 
+            ${req.body.gate !== 'null' ? `'${req.body.gate}'` : 'NULL'}, 
+            ${req.body.next_leg_pointer !== 'null' ? `'${req.body.next_leg_pointer}'` : 'NULL'}, 
+            ${req.body.pax !== 'null' && req.body.pax !== '' ? `'${req.body.pax}'` : 'NULL'},
+            '${req.body.remarks}', 
+            ${req.body.scheduled_arrival_time === '' ? 'NULL' : startOfDayArrival.unix() + moment.duration(req.body.scheduled_arrival_time).asSeconds()}, 
+            ${req.body.scheduled_departure_time === '' ? 'NULL' : startOfDayArrival.unix() + moment.duration(req.body.scheduled_departure_time).asSeconds()},
+            ${req.body.wheelchair_count !== 'null' && req.body.wheelchair_count !== '' ? `'${req.body.wheelchair_count}'` : 'NULL'},
+            ${req.body.isSubservice},
+            ${req.body.flightStatus},
+            CONCAT('ADHC-', (@autoincrement := AUTO_INCREMENT) + 1)
+        FROM information_schema.TABLES
+        WHERE TABLE_SCHEMA = "ultravi_ulav"
+        AND TABLE_NAME = "${tablename}";
+    `;
 
         console.log(query);
                 
