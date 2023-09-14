044eb2cd4e833f5f00620c62d3af9abed5ee40aa
mdambrosioultravi <mdambrosio@ultravi.com>
2023-09-14 15:47:34 -0400

    Testing adding {} to empty log files.

--- logger/index.js
+++ logger/index.js
@@ -36,35 +36,35 @@ const writeQueue = new WriteQueue();
 
 // Usage
 async function writeToLogFile(data, arrayName) {
-    const operation = async () => {
-      const fullPath = path.resolve('logs/' + getFileName());
-  
-      try {
-        // Read existing content from the file or create an empty object
-        let logData = {};
-        if (fs.existsSync(fullPath)) {
-          const existingContent = await fs.promises.readFile(fullPath, 'utf8');
-          logData = JSON.parse(existingContent);
-        }
-  
-        // Initialize the array if it doesn't exist
-        if (!logData[arrayName]) {
-          logData[arrayName] = [];
-        }
-        
-        // Append data to the specified array
-        logData[arrayName].push(data);
-  
-        // Write updated content back to the file
-        await fs.promises.writeFile(fullPath, JSON.stringify(logData, null, 2));
-  
-        console.log('Data appended to array:', arrayName);
-      } catch (error) {
-        console.error('Error writing to file:', error);
+  const operation = async () => {
+    const fullPath = path.resolve('logs/' + getFileName());
+
+    try {
+      // Read existing content from the file or create an empty object
+      let logData = {};
+      if (fs.existsSync(fullPath)) {
+        const existingContent = await fs.promises.readFile(fullPath, 'utf8');
+        logData = JSON.parse(existingContent);
       }
-    };
-  
-    writeQueue.enqueue(operation);
+
+      // Initialize the array if it doesn't exist
+      if (!logData[arrayName]) {
+        logData[arrayName] = [];
+      }
+
+      // Append data to the specified array
+      logData[arrayName].push(data);
+
+      // Write updated content back to the file
+      await fs.promises.writeFile(fullPath, JSON.stringify(logData, null, 2));
+
+      console.log('Data appended to array:', arrayName);
+    } catch (error) {
+      console.error('Error writing to file:', error);
+    }
+  };
+
+  writeQueue.enqueue(operation);
 }
 
 function getFileName(){
