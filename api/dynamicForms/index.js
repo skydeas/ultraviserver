35e2d22c4d8566552220e92d15d689c70f9daf45
mdambrosioultravi <mdambrosio@ultravi.com>
2024-04-23 14:57:58 -0400

    the dynamic add works with airlines table

--- api/dynamicForms/index.js
+++ api/dynamicForms/index.js
@@ -48,6 +48,51 @@ router.post("/getTableSchema", multer().none(), async (req, res) => { // auth.au
 });
 
 /**
+ * Route that creates an add statement for the database based on the schema
+ */
+router.post("/dynamicAdd", express.json(), async (req, res) => { // auth.authenticateRequest(44) multer().none(),
+    // Check if the user is logged in, and if his token is valid, If so, find all tasks they have access    to
+    jwt.verify(req.headers.logintoken, config.privateKey, (err, decoded) => {
+        // If there is a bad token, reject the request.
+        if (err || decoded == undefined) {
+            return res.status(500).send({ message: 'Bad Token' });
+            
+        }
+        // Filter out the 'id' column from the schema
+        const filteredSchema = req.body.schema.filter(column => column.COLUMN_NAME !== 'id');
+        // Extract column names from schema
+        const columnNames = filteredSchema.map(column => column.COLUMN_NAME);
+
+        // Extract values from formData
+        const values = columnNames.map(columnName => {
+            // If formData value is empty string, replace it with NULL
+            const value = req.body.formData[columnName] === '' ? 'NULL' : `'${req.body.formData[columnName]}'`;
+            return value;
+        });
+
+        // Generate column names string for SQL query
+        const columnsString = columnNames.join(', ');
+
+        // Generate values string for SQL query
+        const valuesString = values.join(', ');
+
+        // Construct the INSERT query
+        const insertQuery = `INSERT INTO ${req.body.tableName} (${columnsString}) VALUES (${valuesString})`;
+
+        // console.log(getTableSchemaQuery)
+        //  [req.body.remarks !== 'null' && req.body.remarks !== '' ? req.body.remarks : null,]
+        connectionPool.query(insertQuery, (err, response) => {
+            if (err) {
+                console.log("Query Error: ", err);
+                return res.status(500).send({ message: 'Internal Server Error' });
+            }
+            console.log(response);
+            res.status(200).send({message: 'success', response: response});
+        }); 
+    })
+});
+
+/**
  * Route that returns the data of a table.
  */
 router.post("/getDropdownOptions", multer().none(), async (req, res) => { // auth.authenticateRequest(44)
@@ -73,6 +118,8 @@ router.post("/getDropdownOptions", multer().none(), async (req, res) => { // aut
         }); 
     })
 });
+
+
 
 /** selectAllAdditionalServicesWithFilter
  *  Route to get a specific additional service using the flight id of the modal as the identifier.
