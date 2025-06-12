224bd8572cd5ab49b45c8a2613c44804c8f648ff
Ultra Dev <mdambrosio@ultravi.com>
2025-06-12 15:42:04 -0400

    added OpHazardForm getAll and view by id

--- api/safety/index.js
+++ api/safety/index.js
@@ -66,4 +66,24 @@ router.post("/createOpHazardForm", auth.authenticateRequest(57), multer().none()
     })
 });
 
+router.get("/getAllOpHazardForm", auth.authenticateRequest(60), async (req, res) => {
+    // now get a Promise wrapped instance of that connectionPool
+
+    let response = await connectionPool.promise().execute(config.queries.getAllOpHazardForm);
+
+    // The response is in the format of ([data],[buff]); We will return both since we handle taking only the data in the role service
+    res.json(response[0]);
+});
+
+router.get("/getAllOpHazardFormById/:id", auth.authenticateRequest(60), async (req, res) => {
+    // now get a Promise wrapped instance of that connectionPool
+    
+    let response = await connectionPool.promise().execute(config.queries.getAllOpHazardFormId, [req.params.id]);
+    console.log(response)
+    // The response is in the format of ([data],[buff]); We will return both since we handle taking only the data in the role service
+    res.json(response[0]?.[0]);
+});
+
+
+
 module.exports = router;
